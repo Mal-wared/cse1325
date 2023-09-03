4dd5e40486b3a73185b308f40bea8e4cb66a72e2
Nicholas Tran <nvt7150@mavs.uta.edu>
2023-09-02 21:31:23 -0500

    P02 full_credit

---
+++ P02/full_credit/Color.java
@@ -0,0 +1,3 @@
+public enum Color{
+	PURPLE(0), HOT_PINK(1), LIME_GREEN(2), PINK(3)
+}
---
+++ P02/full_credit/TestLine.java
@@ -0,0 +1,6 @@
+public class TestLine{
+	public static void main(String[] args){
+		Line purple = new Line(6.111, 4,234, 1.234, 5.678, PURPLE);
+		purple.toString();
+	}
+}
