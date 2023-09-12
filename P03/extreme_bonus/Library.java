a32d84ecdfdd680936c8ff3290d14134c6e0c690
Nicholas Tran <nvt7150@mavs.uta.edu>
2023-09-12 01:55:31 -0500

    P03 extreme_bonus

---
+++ P03/extreme_bonus/Library.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Library{
+	private String name;
+	private ArrayList<Publication> publications = new ArrayList<>();
+	private ArrayList<Patron> patrons = new ArrayList<>();
+	
+	public Library(String name){
+		this.name = name;
+	}
+	
+	public void addPublication(Publication publication){
+		publications.add(publication);
+	}
+	
+	public void addPatron(Patron patron){
+		patrons.add(patron);
+	}
+	
+	public String patronMenu(){
+		StringBuilder patMenu = new StringBuilder();
+		patMenu.append(String.format("Patrons\n\n"));
+		for(int i = 0; i < patrons.size(); i++){
+			patMenu.append(String.format("%d) %s\n", i, patrons.get(i).toString()));
+		}
+		return patMenu.toString();
+	}
+	
+	public void checkOut(int publicationIndex, int patronIndex){
+		publications.get(publicationIndex).checkOut(patrons.get(patronIndex));
+		
+	}
+	
+	public String toString(){
+		StringBuilder toString = new StringBuilder();
+		toString.append(String.format("%s\n\n", name));
+		for(int i = 0; i < publications.size(); i++){
+			toString.append(String.format("%d) %s", i, publications.get(i).toString()));
+		}
+	
+		return toString.toString();
+	}
+}
---
+++ P03/extreme_bonus/Publication.java
@@ -0,0 +1,42 @@
+import java.time.LocalDate;
+
+public class Publication{
+	private String title;
+	private String author;
+	private int copyright;
+	private Patron loanedTo;
+	private LocalDate dueDate;
+	
+	public Publication(String title, String author, int copyright){
+		this.title = title;
+		this.author = author;
+		if(copyright < 1900 || copyright > LocalDate.now().getYear()){
+			throw new IllegalArgumentException("Invalid date");
+		}
+		this.copyright = copyright;
+	}
+	
+	public void checkOut(Patron patron){
+		loanedTo = patron;
+		dueDate = LocalDate.now().plusDays(14);
+	}
+	
+	public void checkIn(){
+		
+	}
+	
+	@Override
+	public String toString(){
+		String loanStatus = new String();
+		String toString = new String();
+		if(loanedTo != null){
+			loanStatus = String.format("    --> loaned to %s until %s\n", loanedTo, dueDate.toString());
+			toString = String.format("%s's \"%s\", copyright %d\n%s", author, title, copyright, loanStatus);
+		}
+		else{
+			toString = String.format("%s's \"%s\", copyright %d\n", author, title, copyright);
+		}
+		
+		return toString;
+	}
+}
