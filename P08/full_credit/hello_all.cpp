de43c449853638739f10ef77462a7586d22a1fc5
Nicholas Tran <nvt7150@mavs.uta.edu>
2023-10-24 18:12:49 -0500

    P08 hello world finished

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	std::cout << "What's your name? ";
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout << "Hello, " << name << "!" << std::endl;
+}
---
+++ P08/full_credit/hello_me.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Hello, Nicholas Tran!" << std::endl;
+}
