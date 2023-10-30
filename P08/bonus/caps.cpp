78b23daeb7576aa1021efd836e07dc5d157b8850
Nicholas Tran <nvt7150@mavs.uta.edu>
2023-10-29 21:16:18 -0500

    P08 bonus completion

--- P08/bonus/caps.cpp
+++ P08/bonus/caps.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <cctype>
 
 int main(int argc, char* argv[]){
     std::vector<std::string> caps;
     std::vector<std::string>* no_caps = new std::vector<std::string>;
 
-    for(int i = 0; i < argc; ++i){
-        caps.push_back(std::string(argv[i]));
-        no_caps->push_back(std::string(argv[i]));
+    for(int i = 1; i < argc; i++){
+        if(std::isupper(argv[i][0])){
+            caps.push_back(std::string(argv[i]));
+        }
+        else{
+            no_caps->push_back(std::string(argv[i]));
+        }
     }
 
     std::cout << "Capitalized:" << std::endl;
@@ -17,7 +21,7 @@ int main(int argc, char* argv[]){
         std::cout << cap << std::endl;
     }
 
-    std::cout << "Lower Case:" << std::endl;
+    std::cout << "\nLower Case:" << std::endl;
     for(std::string no_cap : *no_caps){
         std::cout << no_cap << std::endl;
     }
