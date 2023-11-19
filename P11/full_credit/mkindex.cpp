97c04bf06181854944853a28ebeb090a0e618847
Nicholas Tran <nvt7150@mavs.uta.edu>
2023-11-18 23:55:28 -0600

    P11 full_credit complete

--- P11/full_credit/mkindex.cpp
+++ P11/full_credit/mkindex.cpp
@@ -1,24 +1,33 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <cctype>
 #include "Index.h"
 
 int main(int argc, char* argv[]){
     Index userIndex;
 
-    std::string line;
-    
-    for(int i = 1; i < argc; i++){
-        std::ifstream file(argv[i]);
-        int lineNum = 0;
+    for(int i = 1; i < argc; i++){ 
+        std::ifstream file(argv[i]); 
+        int lineNum = 1;
+
+        std::string line;
         while(std::getline(file, line)){
             std::istringstream iss(line);
             std::string word;
 
             while(iss >> word){
+                if(!std::isalpha(word.back())){
+                    word.pop_back();
+                }
+
+                for(char& c : word){
+                    c = std::tolower(c);
+                }
+
                 userIndex.add_word(word, argv[i], lineNum);
-                lineNum++;
             }
+            lineNum++;
         }
     }
     std::cout << userIndex;
